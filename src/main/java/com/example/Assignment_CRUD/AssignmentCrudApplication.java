0997d500fec43bd09f4924ff00f4bfdd05bb2b35
Junt Surani <junitsurani@gmail.com>
2024-05-31 21:49:41 +0530

    Project Created

---
+++ src/main/java/com/example/Assignment_CRUD/AssignmentCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assignment_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Assignment_CRUD/AssignmentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assignment_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
