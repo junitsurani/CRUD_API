77ec84b1e7c7022d26235e06fd60860e897a9e24
Junt Surani <junitsurani@gmail.com>
2024-05-31 22:34:41 +0530

    Project Created

---
+++ src/main/java/com/example/assignment_crud/AssignmentCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentCrudApplication.class, args);
+	}
+
+}
